2800b1d5f7637bed23208e3b7e45406248af5794
Avram Virgil <av.virgil@gmail.com>
2018-10-04 22:39:10 +0100

    updater will also include the secondary command for my test

--- omv-theme-update.sh
+++ omv-theme-update.sh
@@ -3,8 +3,12 @@
 rm -rf /root/omv-theme/
 git clone https://github.com/virgil-av/omv-theme.git
 cd /root/omv-theme
+
 chmod +x omv-theme.sh
 chmod +x update-theme.sh
+chmod +x omv-theme2.sh
+
 rm -rf /usr/bin/omv-theme
 cp /root/omv-theme/omv-theme.sh /usr/bin/omv-theme
+cp /root/omv-theme/omv-theme2.sh /usr/bin/omv-theme2
 exec omv-theme
