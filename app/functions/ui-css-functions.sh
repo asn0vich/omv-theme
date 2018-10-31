8e3912fd37b5cc55c6cedba6f1d6f5b7e091c545
Avram Virgil <av.virgil@gmail.com>
2018-10-31 23:51:46 +0000

    well this should make everything work

--- app/functions/ui-css-functions.sh
+++ app/functions/ui-css-functions.sh
@@ -13,10 +13,10 @@ COLOR_CODE=$(whiptail --inputbox "Insert color hex or color name, example of hex
 exitstatus=$?
 if [ $exitstatus = 0 ]; then
     echo "User selected Ok and entered " $COLOR_CODE
-    style="\\n\n div#header{background: $COLOR_CODE !important; /*custom-background-color*/}"
+    style="div#header{background: $COLOR_CODE !important; /*custom-background-color*/}"
     cp /var/www/openmediavault/css/theme-triton.min.css /root/omv-theme/backup/header-bg-color.bak
-    # echo $style >> /var/www/openmediavault/css/theme-triton.min.css
-    sed -i "$ a \\\n $style" /var/www/openmediavault/css/theme-triton.min.css
+    sed -i "$ a \\\n" /var/www/openmediavault/css/theme-triton.min.css
+    sed -i "$ a $style" /var/www/openmediavault/css/theme-triton.min.css
 else
     echo "User selected Cancel."
 fi
@@ -36,6 +36,7 @@ revert_header_background_color() {
 #
 
 set_remove_header() {
+    sed -i "$ a \\\n" /var/www/openmediavault/css/theme-triton.min.css
     sed -i -e "/$/r /root/omv-theme/css/remove-header.css" /var/www/openmediavault/css/theme-triton.min.css
 }
 
