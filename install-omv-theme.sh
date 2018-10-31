2b42fb7c069d852c8c98b744af7f6f2f805a806d
Avram Virgil <av.virgil@gmail.com>
2018-10-31 13:14:35 +0000

    attempt to execute test executable

--- install-omv-theme.sh
+++ install-omv-theme.sh
@@ -10,7 +10,9 @@ chmod +x omv-theme2.sh
 
 cp omv-theme.sh /usr/bin/omv-theme
 cp omv-theme-update.sh /usr/bin/omv-theme-update
-cp omv-theme2.sh /usr/bin/omv-theme2
+
+echo "#!/bin/sh" >> /usr/bin/omv-theme2
+echo "./root/omv-theme/omv-theme2" >> /usr/bin/omv-theme2
 
 cd
 exec omv-theme
