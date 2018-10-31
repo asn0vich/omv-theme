b5cbec8fb2048f5bd8535863e2b72e41b0fd3352
Avram Virgil <av.virgil@gmail.com>
2018-10-31 17:13:31 +0000

    releasing background color for the main

--- app/omv-main.sh
+++ app/omv-main.sh
@@ -5,6 +5,7 @@ source ./functions/menu-configs.sh
 source ./functions/menu-functions.sh
 source ./functions/themes-functions.sh
 source ./functions/ui-js-functions.sh
+source ./functions/ui-css-functions.sh
 
 # menu imports
 source ./menues/themes-submenu.sh
