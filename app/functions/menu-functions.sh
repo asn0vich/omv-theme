9d7b3b2d6a8d03bfa6dfa54a3f2b6c20d90029a7
Avram Virgil <av.virgil@gmail.com>
2018-10-31 15:23:07 +0000

    super annoying stuff

--- app/functions/menu-functions.sh
+++ app/functions/menu-functions.sh
@@ -26,7 +26,29 @@ do_update_omv_theme() {
   do_omv_triton
   # do update
   cd ~
-  /bin/bash update-theme.sh
+  # remove old code and pull the new one
+  rm -rf /root/omv-theme
+  git clone https://github.com/virgil-av/omv-theme.git
+  cd /root/omv-theme
+
+  # make main scripts executable
+  chmod +x omv-theme.sh
+  chmod +x update-theme.sh
+  chmod +x omv-theme2.sh
+
+  # clean scripts from /usr/bin
+  rm -r /usr/bin/omv-theme
+  rm -r /usr/bin/omv-theme2
+  rm -r /usr/bin/update-theme
+
+  # copy and create scripts for execution
+  cp /root/omv-theme/omv-theme.sh /usr/bin/omv-theme
+  cp /root/omv-theme/update-theme.sh /usr/bin/update-theme
+
+  echo "#!/bin/bash" >> /usr/bin/omv-theme2
+  echo "/bin/bash /root/omv-theme/omv-theme2" >> /usr/bin/omv-theme2
+
+  exec omv-theme
 }
 
 do_uninstall() {
