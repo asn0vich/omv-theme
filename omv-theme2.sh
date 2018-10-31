c2c0e9d685a640a33f89358ebc8a59079a67b3e1
Avram Virgil <av.virgil@gmail.com>
2018-10-31 13:20:26 +0000

    paths are killing me

--- omv-theme2.sh
+++ omv-theme2.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
 # function imports
-source ./components/menu-configs.sh
-source ./components/themes-functions.sh
-source ./components/ui-js-functions.sh
-source ./components/ui-css-functions.sh
+source components/menu-configs.sh
+source components/themes-functions.sh
+source components/ui-js-functions.sh
+source components/ui-css-functions.sh
 
 # menu imports
-source ./menues/themes-submenu.sh
-source ./menues/js-plugins-submenu.sh
-source ./menues/css-plugins-submenu.sh
-source ./menues/main-menu.sh
+source menues/themes-submenu.sh
+source menues/js-plugins-submenu.sh
+source menues/css-plugins-submenu.sh
+source menues/main-menu.sh
 
 open_main_menu
 
