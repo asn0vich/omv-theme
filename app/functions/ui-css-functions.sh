67388c899805fdcee12398458bde7976c990d53e
Avram Virgil <av.virgil@gmail.com>
2018-11-22 16:16:23 +0000

    eh you need a generic name did you forget

--- app/functions/ui-css-functions.sh
+++ app/functions/ui-css-functions.sh
@@ -55,7 +55,7 @@ if [ $exitstatus = 0 ]; then
     wget $BGIMG_URL -O /root/omv-theme/images/custom-background.png
     cp /root/omv-theme/images/custom-background.png /var/www/openmediavault/images/custom-background.png
 
-    sed -i "/background-image:/c\background-image: url($BGIMG_URL);" /root/omv-theme/css/background-image.css
+    sed -i "/background-image:/c\background-image: url(../custom-background.png);" /root/omv-theme/css/background-image.css
     sed -i "$ a \\\n\n\n" /var/www/openmediavault/css/theme-triton.min.css
     sed -i -e "/ /r /root/omv-theme/css/background-image.css" /var/www/openmediavault/css/theme-triton.min.css
 else
