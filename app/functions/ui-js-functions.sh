eff8fe85ce6c822970ecc164a5d4dd3c6307b88d
Avram Virgil <av.virgil@gmail.com>
2018-12-17 13:29:54 +0000

    wrong folder

--- app/functions/ui-js-functions.sh
+++ app/functions/ui-js-functions.sh
@@ -68,9 +68,9 @@ rm /var/www/openmediavault/images/custom-logo.png
 }
 
 do_snow() {
-cp /root/omv-theme/javascript/let-it-snow.js /var/www/openmediavault/js/omv/let-it-snow.js
+cp /root/omv-theme/javascript/let-it-snow.js /var/www/openmediavault/js/omv/module/public/let-it-snow.js
 }
 
 do_revert_snow() {
-rm -rf /var/www/openmediavault/js/omv/let-it-snow.js
+rm -rf /var/www/openmediavault/js/omv/module/public/let-it-snow.js
 }
