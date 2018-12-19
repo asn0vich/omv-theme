e1fe42e1a2c1a7e4dfad4cfeb0349964ca1ea76d
Avram Virgil <av.virgil@gmail.com>
2018-12-19 14:32:17 +0000

    it was working before too damn

--- app/functions/ui-css-functions.sh
+++ app/functions/ui-css-functions.sh
@@ -71,21 +71,14 @@ echo "(Exit status was $exitstatus)"
 
 set_font_size_and_weight() {
 FONT_SIZE=$(whiptail --inputbox "It should be a number" 8 78 "12" --title "Font size" 3>&1 1>&2 2>&3)
+FONT_WEIGHT=$(whiptail --inputbox "Can be a number (100-900) or text normal, bold, bolder, lighter, google font-weight property values" 8 78 "bold" --title "Font Weight" 3>&1 1>&2 2>&3)
 
 exitstatus=$?
 if [ $exitstatus = 0 ]; then
-    FONT_WEIGHT=$(whiptail --inputbox "Can be a number (100-900) or text normal, bold, bolder, lighter, google font-weight property values" 8 78 "bold" --title "Font Weight" 3>&1 1>&2 2>&3)
 
-    if [ $exitstatus = 0 ]; then
-        echo $FONT_SIZE
-        echo $FONT_WEIGHT
+    sed -i -e "/font-size:/,/;/c\ font-size:$FONT_SIZE;" /root/omv-theme/css/font.css
+    sed -i -e "/font-weight:/,/;/c\ font-size:$FONT_WEIGHT;" /root/omv-theme/css/font.css
 
-         sed -i -e "/font-size:/,/;/c\ font-size:$FONT_SIZE;" /root/omv-theme/css/font.css
-         sed -i -e "/font-weight:/,/;/c\ font-size:$FONT_WEIGHT;" /root/omv-theme/css/font.css
-
-    else
-        echo "User selected Cancel."
-    fi
 else
     echo "User selected Cancel."
 fi
