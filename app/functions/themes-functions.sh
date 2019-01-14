03333561a2bba7f0713458336469fb4f5a1012b5
Avram Virgil <av.virgil@gmail.com>
2019-01-14 13:27:30 +0000

    change the way we apply the theme, also this time we also clear the cache too.

--- app/functions/themes-functions.sh
+++ app/functions/themes-functions.sh
@@ -10,16 +10,18 @@ fi
 }
 
 remove_custom_theme() {
-    echo 'OMV_WEBUI_THEME=triton' >> /etc/default/openmediavault
     rm -r /var/www/openmediavault/css/theme-custom.*.css
-    cp /root/omv-theme/backup/controlpanelabstract.inc /usr/share/php/openmediavault/controlpanel/controlpanelabstract.inc
+    rm -r /var/www/openmediavault/css/theme-custom.css
+#    cp /root/omv-theme/backup/controlpanelabstract.inc /usr/share/php/openmediavault/controlpanel/controlpanelabstract.inc
 }
 
 apply_theme(){
     themeName=$1
 
-    echo 'OMV_WEBUI_THEME=triton' >> /etc/default/openmediavault
     rm -r /var/www/openmediavault/css/theme-custom.*.css
-    cp /root/omv-theme/themes/$themeName.css /var/www/openmediavault/css/theme-custom.$themeName.css
-    sed -i "/theme-custom/c\$fileName = \"css\/theme-custom.$themeName.css\";" /usr/share/php/openmediavault/controlpanel/controlpanelabstract.inc
+    rm -r /var/www/openmediavault/css/theme-custom.css
+    cp /root/omv-theme/themes/$themeName.css /var/www/openmediavault/css/theme-custom.css
+#    sed -i "/theme-custom/c\$fileName = \"css\/theme-custom.$themeName.css\";" /usr/share/php/openmediavault/controlpanel/controlpanelabstract.inc
+    source /usr/share/openmediavault/scripts/helper-functions && omv_purge_internal_cache
+
 }
