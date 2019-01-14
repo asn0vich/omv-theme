2875f129869a331cfba4cc6ad3b1d9b5073c1399
Avram Virgil <av.virgil@gmail.com>
2019-01-14 13:55:58 +0000

    not equal in bash...

--- app/functions/themes-functions.sh
+++ app/functions/themes-functions.sh
@@ -10,7 +10,7 @@ apply_theme(){
     rm -rf /var/www/openmediavault/css/theme-custom.*
 
     #clean way to apply or remove custom theme, if default return back to triton theme also set file owner to omv
-    if [ $THEME_NAME -ne "default" ]; then
+    if [ $THEME_NAME != "default" ]; then
         cp /root/omv-theme/themes/$THEME_NAME.css /var/www/openmediavault/css/theme-custom.css
         chown openmediavault-webgui:openmediavault-webgui /var/www/openmediavault/css/theme-custom.css
     fi
