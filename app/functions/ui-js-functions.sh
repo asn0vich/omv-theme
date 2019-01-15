649f0217c2f72164a9a8cc63661ad11fc19eaf8f
Avram Virgil <av.virgil@gmail.com>
2019-01-15 10:10:42 +0000

    moving things around in the function, trying to clean it as much as possible

--- app/functions/ui-js-functions.sh
+++ app/functions/ui-js-functions.sh
@@ -56,21 +56,21 @@ LOGO_URL=$1
 
 if [ -z "$LOGO_URL" ]; then
     LOGO_URL=$(whiptail --inputbox "Insert logo url [hotlink to image should end in (jpg, png)]" 8 78 http:// --title "Set logo url" 3>&1 1>&2 2>&3)
+    exitstatus=$?
 fi
 
-exitstatus=$?
 if [ $exitstatus = 0 ] || [ -n "$LOGO_URL" ]; then
-    echo "User selected Ok and entered " $LOGO_URL
+    echo "User added logo url:" $LOGO_URL
     wget $LOGO_URL -O /root/omv-theme/images/custom-logo.png
     cp /root/omv-theme/images/custom-logo.png /var/www/openmediavault/images/custom-logo.png
     sed -i -e '/buildHeader: function() {/,/},/c\buildHeader: function() {\n\/\/custom header\n},' /var/www/openmediavault/js/omv/workspace/Workspace.js
     sed -i -e "/\/\/custom header/r /root/omv-theme/javascript/header-logo.js" /var/www/openmediavault/js/omv/workspace/Workspace.js
     cat /root/omv-theme/css/header-logo.css >> /var/www/openmediavault/css/theme-triton.min.css
 else
-    echo "User selected Cancel."
+    echo "User did not want to add logo url"
 fi
 
-echo "(Exit status was $exitstatus)"
+echo "(Set custom logo has been applied)"
 }
 
 
